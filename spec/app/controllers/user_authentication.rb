5e0ab09b363ccb2de8de28c4f294796cfb2ecf9b
Liberal <liberal@Liberal.local>
2015-07-11 17:57:13 +0430

    Create Spec for UserAuthenticationController

--- spec/app/controllers/user_authentication.rb
+++ spec/app/controllers/user_authentication.rb
@@ -15,7 +15,83 @@ describe UserAuthenticationController do
   describe 'Authenticate User' do
 
     it 'should authenticate user' do
-      binding.pry
+
+      payload = {
+      email: user.email,
+      password: 'password'
+      }
+
+      post 'authentication', payload
+
+      body = JSON.parse response.body
+
+      expect(response.status).to eql 200
+      expect(body['token']).not_to eql nil
+
+    end
+
+    context :failuers do
+
+      context :email do
+
+        it 'should err if email is missing' do
+
+          payload = {
+          # email: user.email,
+          password: 'password'
+          }
+
+          post 'authentication', payload
+
+          expect(response.status).to eql 401
+
+        end
+
+        it 'should err if email is invalid' do
+
+          payload = {
+          email: 'wrong@wrong.com',
+          password: 'password'
+          }
+
+          post 'authentication', payload
+
+          expect(response.status).to eql 401
+
+        end
+
+      end
+
+      context :password do
+
+        it 'should err if password is missing' do
+
+          payload = {
+          email: user.email,
+          # password: 'password'
+          }
+
+          post 'authentication', payload
+
+          expect(response.status).to eql 401
+
+        end
+
+        it 'should err if password is not valid' do
+
+          payload = {
+          email: user.email,
+          password: 'invalid'
+          }
+
+          post 'authentication', payload
+
+          expect(response.status).to eql 401
+
+        end
+
+      end
+
     end
 
   end
